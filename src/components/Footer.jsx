cc68afed2acd78105bb7e799030228f32f1e447e
Clyvia Alves <clyvia.kads@gmail.com>
2021-02-17 14:39:01 -0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return (
+        <footer>
+            <p>Feito com amor por Clyvia, {year}</p>
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1><MenuBookIcon /> Su Notes 🦄 </h1>
+    </header>
+  );
+};
+
+export default Header;
